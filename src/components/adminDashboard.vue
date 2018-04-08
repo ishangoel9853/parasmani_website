80b3bb4d49d8e1d2fedad6a67a39f37c71bb551c
Ishan <ishangoel9853@gmail.com>
2018-04-08 20:03:43 +0530

    added viewQues page

--- src/components/adminDashboard.vue
+++ src/components/adminDashboard.vue
@@ -45,7 +45,7 @@
             <v-icon>account_box</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <router-link to="/viewQuestions">View Questions</router-link>
+            <router-link to="/viewQues">View Questions</router-link>
           </v-list-tile-content>
         </v-list-tile>
         <v-list-tile @click="">
