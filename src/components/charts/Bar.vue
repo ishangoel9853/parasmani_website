9146377ab847ee3a2c75c7ad63b1225d4df08136
kanishkarj <kanishkarj@hotmail.com>
2018-04-08 20:07:54 +0530

    Student Analytics

---
+++ src/components/charts/Bar.vue
@@ -0,0 +1,11 @@
+<script>
+import { HorizontalBar } from 'vue-chartjs'
+
+export default {
+  extends: HorizontalBar,
+    props: ['data', 'options'],
+    mounted () {
+      this.renderChart(this._props.data, this._props.options)
+    }
+}
+</script>
---
+++ src/components/charts/Doughnut.vue
@@ -0,0 +1,11 @@
+<script>
+import { Doughnut } from 'vue-chartjs'
+
+export default {
+  props: ['data', 'options'],
+  extends: Doughnut,
+  mounted () {
+    this.renderChart(this._props.data, this._props.options)
+  }
+}
+</script>
