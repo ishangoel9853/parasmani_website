7cc3ce0633ea37e11a7a8af80701ef57f57927ff
Ishan <ishangoel9853@gmail.com>
2018-04-09 21:50:37 +0530

    updated view Ques page

--- src/components/viewQues.vue
+++ src/components/viewQues.vue
@@ -1,3 +1,4 @@
+<!--
 <template lang="html">
   <div id="app">
  <ul id="example-2">
@@ -9,31 +10,91 @@
  </ul>
 </div>
 </template>
+-->
+
+<template>
+<div>
+  <v-select
+    label="Language ID"
+    v-model="langID"
+    :items="langList"
+    :rules="[v => !!v || 'Language is required']"
+    required
+  ></v-select>
+
+  <v-select
+    label="Subject"
+    v-model="subject"
+    :items="subjectList"
+    :rules="[v => !!v || 'Subject is required']"
+    required
+  ></v-select>
+
+
+  <v2-table :data="quesList">
+
+  <!-- <v2-table-column label="Language" prop="name"></v2-table-column>
+
+    <v2-table-column label="Subject" prop="date"></v2-table-column>  -->
+    <v2-table-column label="ID" prop="ID"></v2-table-column>
+
+    <v2-table-column label="Question" prop="Title"></v2-table-column>
+
+    <v2-table-column label="Opt1" prop="Opt1"></v2-table-column>
+    <v2-table-column label="Opt2" prop="Opt2"></v2-table-column>
+    <v2-table-column label="Opt3" prop="Opt3"></v2-table-column>
+    <v2-table-column label="Opt4" prop="Opt4"></v2-table-column>
+    <v2-table-column label="Set" prop="Set"></v2-table-column>
+
+    <v2-table-column label="Correct" prop="Correct"></v2-table-column>
+  </v2-table>
+
+  Enter the ID of the question to be removed:
+  <v-text-field
+    label="quesID"
+    v-model="quesID"
+    :rules="[v => !!v || 'Please enter a question ID.']"
+    required
+  ></v-text-field>
+  <v-btn @click="remove(quesID)">Delete</v-btn>
+
+  </div>
+
+</template>
 
 <script>
 export default {
 
  data() {
    return {
-   books: [
-     { name : 'The Majesty of Vue.js', author : 'Alex Kyriakidis, Kostas Maniatis and Evan You' , completed : false },
-     { name : 'Laravel: Code Bright ', author : 'Dayle Rees' , completed : false },
-     { name : 'Laravel: From Apprentice To Artisan', author : 'Taylor Otwell' , completed : false },
-     { name : 'Laravel Testing Decoded', author : 'JeffreyWay' , completed : false }
-   ]
+     langID: 0,
+     langList: [],
+     subject: null,
+     subjectList: [],
+     quesList: [],
+     quesID: null,
   }
 },
  methods: {
-   isNotComplete: function(book){
-     return book.completed == false
-   },
-   remove: function(book){
-     console.log(book);
 
-     var idx = this.books.indexOf(book)
+   remove: function(ques){
+     console.log(ques);
+
+     var idx = this.quesList.indexOf(ques)
      console.log('idx', idx)
-     this.books.splice(idx,1)
+     this.quesList.splice(idx,1)
    }
+ },
+ mounted: function() {
+   const self = this
+   axios.AuthAxios.get('/languages').then((response) => {
+     self.langList = response.data
+   })
+   axios.AuthAxios.get('/subjects').then((response) => {
+     response.data.forEach(function(val) {
+       self.subjectList.push(val.title)
+     })
+   })
  }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+
+import 'beautify-scrollbar/dist/index.css';
+import 'v2-table/dist/index.css';
+import V2Table from 'v2-table';
+
 import actions from './modules/actions'
 import state from './modules/state'
 import mutations from './modules/mutations'
 import getters from './modules/getters'
 
 Vue.use(Vuex)
+Vue.use(V2Table)
 
 export default new Vuex.Store({
   state,
