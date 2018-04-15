28270c8d2b3dd4f6f68cd9dd7e632965d3907908
kanishkarj <kanishkarj@hotmail.com>
2018-04-15 15:21:07 +0530

    url update

--- src/axios.js
+++ src/axios.js
@@ -6,15 +6,15 @@ export default {
   AuthAxios : axios.create({
     // baseURL: 'https://intense-scrubland-78603.herokuapp.com/',
     // baseURL: 'http://10.250.6.252:4000/',
-    baseURL: 'http://localhost:4000/',
-    // baseURL: 'https://intense-scrubland-78603.herokuapp.com/',
+    // baseURL: 'http://localhost:4000/',
+    baseURL: 'https://intense-scrubland-78603.herokuapp.com/',
     headers: {
       'Authorization':`Bearer ${window.localStorage.getItem("token")}`
     }
   }),
   NotAuthAxios : axios.create({
-    baseURL: 'http://localhost:4000/',
-      // baseURL: 'https://intense-scrubland-78603.herokuapp.com/',
+    // baseURL: 'http://localhost:4000/',
+      baseURL: 'https://intense-scrubland-78603.herokuapp.com/',
       /* other custom settings */
   })
 }
